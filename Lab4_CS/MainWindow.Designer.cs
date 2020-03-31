c84f963e88c28f7cf9e341fdadd7070d2b98c7c7
Ilya Levinskii <56347883+DeppaRet@users.noreply.github.com>
2020-03-31 14:01:09 +0300

    Initial commit

---
+++ Lab4_CS/MainWindow.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Lab4_CS
+{
+  partial class MainWindow
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.Table = new System.Windows.Forms.DataGridView();
+      ((System.ComponentModel.ISupportInitialize)(this.Table)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // Table
+      // 
+      this.Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.Table.Location = new System.Drawing.Point(10, 74);
+      this.Table.Name = "Table";
+      this.Table.Size = new System.Drawing.Size(505, 318);
+      this.Table.TabIndex = 0;
+      // 
+      // MainWindow
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.Table);
+      this.Name = "MainWindow";
+      this.Text = "MainWindow";
+      this.Load += new System.EventHandler(this.MainWindow_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.Table)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Table;
+    }
+}
+
